bcd75de47302e11ed43d80b1f49c2c929d54ac19
subashchanderoffice01-maker <subashchanderoffice01@gmail.com>
2026-07-11 17:12:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -114,7 +114,7 @@ dots.forEach((dot, index) => {
 });
 
 setInterval(nextSlide, 5000);
-const scriptURL = "https://script.google.com/macros/s/AKfycbwvj5mlQDRLZdqeQQX-lbUKcFsljEpJ-lQmXd6WKV09AA2SV9b8dyXnR5D8eIBFxSnL/exec";
+const scriptURL = "https://script.google.com/macros/s/AKfycby6-IaVxIhKOwYNzXpvB-Iv0gmcwtfJn2lHkogeCbvQDxHYKCrRkyKU1TbEpQ8ubLHV/exec";
 
 document
 .getElementById("contactForm")
